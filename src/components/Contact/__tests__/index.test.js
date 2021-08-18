9115abcde24dbee41958cbbc9bb5144819ccdb09
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-18 13:32:24 -0400

    added contact form and test

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact component', () => {
+    it('renders', () => {
+        render(<ContactForm />);
+    });
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<ContactForm />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    it('matches', () => {
+        const { getByTestId } = render(<ContactForm />);
+        expect(getByTestId('h1tag')).toHaveTextContent('Contact me');
+    });
+});
